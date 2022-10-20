751d17a7be1de86afbd14b73265aab4ad8080972
Ivan M <karlos123jun@hotmail.com>
2022-10-19 19:08:47 -0500

    M

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,26 +4,21 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
     name: {
         type: String,
-        //required: [true,'the name is required']
     },
     firstlastname: {
         type: String,
-        //required: [true,'the first last name is required']
     },
     secondlastname: {
         type: String
     },
     email: {
         type: String,
-        // required: [true,'the email is required']
     },
     pass: {
         type: String,
-        //required: [true,'the pass is required']
     },
     dateofbirth: {
         type: Date ,
-        //required: [true,'the date of birth is required']
     },
     photo: {
         type: String
